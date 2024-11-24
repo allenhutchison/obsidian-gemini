d6431f0bdc3dc93562089128fffe363d6f0b3998
Allen Hutchison <allen@hutchison.org>
2024-11-24 11:42:13 -0800

    Add new prompt templates for general, rewrite, summary, and system prompts; refactor completion prompt handling

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,14 +1,49 @@
 import * as Handlebars from 'handlebars';
-import completionPromptContent from '../prompts/completionPrompt.txt';
+
+// @ts-ignore
+import systemPromptContent from '../prompts/systemPrompt.txt';
+// @ts-ignore
+import completionPromptContent from '../prompts/completionPrompt.txt'; 
+// @ts-ignore
+import generalPromptContent from '../prompts/generalPrompt.txt';
+// @ts-ignore
+import summaryPromptContent from '../prompts/summaryPrompt.txt';
+// @ts-ignore
+import rewritePromptContent from '../prompts/rewritePrompt.txt';
 
 export class GeminiPrompts {
     private completionsPromptTemplate: Handlebars.TemplateDelegate;
+    private systemPromptTemplate: Handlebars.TemplateDelegate;
+    private generalPromptTemplate: Handlebars.TemplateDelegate;
+    private summaryPromptTemplate: Handlebars.TemplateDelegate;
+    private rewritePromptTemplate: Handlebars.TemplateDelegate;
 
     constructor() {
         this.completionsPromptTemplate = Handlebars.compile(completionPromptContent);
+        this.systemPromptTemplate = Handlebars.compile(systemPromptContent);
+        this.generalPromptTemplate = Handlebars.compile(generalPromptContent);
+        this.summaryPromptTemplate = Handlebars.compile(summaryPromptContent);
+        this.rewritePromptTemplate = Handlebars.compile(rewritePromptContent);
     }
 
     completionsPrompt(variables: { [key: string]: string }): string {
         return this.completionsPromptTemplate(variables);
     }
+
+    systemPrompt(variables: { [key: string]: string }): string {
+        return this.systemPromptTemplate(variables);
+    }
+
+    generalPrompt(variables: { [key: string]: string }): string {
+        return this.generalPromptTemplate(variables);
+    }
+
+    summaryPrompt(variables: { [key: string]: string }): string {
+        return this.summaryPromptTemplate(variables);
+    }
+
+    rewritePrompt(variables: { [key: string]: string }): string {
+        return this.rewritePromptTemplate(variables);
+    }
+
 }
