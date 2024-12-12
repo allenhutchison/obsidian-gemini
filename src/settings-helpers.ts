950526c904bd9de83bfd83eb59a636d8593f8956
Allen Hutchison <adh@google.com>
2024-12-11 17:32:51 -0800

    adding support for Gemini 2

--- src/settings-helpers.ts
+++ src/settings-helpers.ts
@@ -22,6 +22,7 @@ export function selectModelSetting(
 		.addDropdown((dropdown) =>
 			dropdown
 				.addOption('gemini-1.5-flash', 'gemini-1.5-flash')
+				.addOption('gemini-2.0-flash-exp', 'gemini-2.0-flash-exp')
 				.addOption('gemini-1.5-pro', 'gemini-1.5-pro')
 				.addOption('gemini-1.5-flash-8b', 'gemini-1.5-flash-8b')
 				.setValue(
